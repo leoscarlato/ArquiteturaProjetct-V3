0b2c8e21f4e9cedceca3ed40fa7d1dba0404dd5e
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2023-04-13 21:14:32 -0300

    começando mensagem

--- demo/src/test/java/com/example/demo/mensagem/MensagemServiceTest.java
+++ demo/src/test/java/com/example/demo/mensagem/MensagemServiceTest.java
@@ -16,8 +16,93 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
 public class MensagemServiceTest {
 
+    @InjectMocks
+    MensagemService mensagemService;
+
+    @Mock
+    MensagemRepository mensagemRepository;
+
+    @Mock
+    UsuarioService usuarioService;
+
+    @Mock
+    MensagemTexto mensagemTexto;
+
+    @Mock
+    MensagemArquivo mensagemArquivo;
+
+
+    @Test
+    void test_todas_mensagens(){
+        MensagemTexto mensagem_1 = new MensagemTexto();
+        MensagemTexto mensagem_2 = new MensagemTexto();
+
+        mensagem_1.setTexto("teste");
+        mensagem_2.setTexto("teste2");
+
+        List<Mensagem> mensagens = new ArrayList<>();
+
+        Mockito.when(mensagemRepository.findAll()).thenReturn(mensagens);
+
+        List<Mensagem> mensagensEncontradas = mensagemService.getAllMensagens();
+
+        Assertions.assertEquals(mensagensEncontradas.size(), mensagens.size());
+    }
+
+    @Test
+    void test_mensagem_get_by_id(){
+        MensagemTexto mensagem_1 = new MensagemTexto();
+        mensagem_1.setTexto("teste");
+        mensagem_1.setId(1);
+
+        Mockito.when(mensagemRepository.findById(1)).thenReturn(Optional.of(mensagem_1));
+
+        Mensagem mensagemEncontrada = mensagemService.getMensagemById(1);
+
+        Assertions.assertEquals(mensagemEncontrada.getId(), mensagem_1.getId());
+    }
+
+    @Test
+    void test_mensagem_get_by_username(){
+        MensagemTexto mensagem_1 = new MensagemTexto();
+        mensagem_1.setTexto("teste");
+        mensagem_1.setId(1);
+
+        Usuario usuario = new Usuario();
+        usuario.setUsername("teste");
+        usuario.setId(1);
+
+        mensagem_1.setUsername(usuario);
+
+        Mockito.when(mensagemRepository.findById(1)).thenReturn(Optional.of(mensagem_1));
+
+        Mensagem mensagemEncontrada = mensagemService.getMensagemById(1);
+
+        Assertions.assertEquals(mensagemEncontrada.getUsername().getId(), usuario.getId());
+    }
+
+    @Test
+    void test_cria_mensagem_texto(){
+        Map<String, String> mensagem = Map.of("texto", "teste");
+
+        Usuario usuario = new Usuario();
+        usuario.setUsername("teste");
+        usuario.setId(1);
+
+        Mockito.when(usuarioService.getUsuarioById(1)).thenReturn(usuario);
+        
+        
+    }
     
+
+
     
 }
